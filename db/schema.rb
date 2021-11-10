3e9910ca5f80d6c705533bd2b765447e7f615e81
axelopez <=>
2021-11-10 17:03:40 -0600

    pg db full

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_10_215402) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
